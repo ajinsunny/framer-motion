7f25bfa52cc946ec70b61af6b633b33dee94b28a
Ajin Sunny <ajin.sunny@gmail.com>
2023-03-16 23:09:08 -0400

    Update App.js
    animation upon clicking the box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./index.css";
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 export default function App() {
+  const [scale, setScale] = useState(1);
+
   const handleClick = () => {
     // Animate the box
-    const box = document.querySelector(".box");
-    box.classList.add("animate");
+    setScale(3);
   };
 
   return (
@@ -13,8 +15,14 @@ export default function App() {
       <motion.div
         className="box"
         onClick={handleClick}
+        initial={{
+          scaleX: 1,
+          scaleY: 0.5,
+        }}
         animate={{
-          scale: [1, 3],
+          scaleX: scale,
+          scaleY: scale,
+          transformOrigin: "top left",
         }}
         transition={{
           duration: 3,
